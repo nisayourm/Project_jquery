8b874d5618ea9309a6188b9c854824ddd0808c37
nysar.yam <nysar.yam@PNC.LAN>
2019-12-09 20:42:08 +0700

    add code

--- js/js.js
+++ js/js.js
@@ -36,7 +36,64 @@ $(document).ready(function(){
          var add = $('#number').val();
          addNumber(add);
      });
+     
+    // wertyuiop[]qwertyuiopwertyuiopweyuioqwertyuiopwertyuiopwertiop
+    $(document).ready(() => {
+        requestApi();
+    });
     
+    //reques api
+    var requestApi = () => {
+        $.ajax({
+            dataType: 'json',
+            url: getUrl(),
+            success: (data) => getData(data),
+            error: () => getError(),
+        });
+    };
+    
+    //get url
+    var getUrl = () => {
+        var url = "https://raw.githubusercontent.com/radytrainer/test-api/master/test.json";
+        return url;
+    };
+    
+    //get data
+    var getData = (myData) => {
+        myData.recipes.forEach(element => {
+            //recipe element.ingredients
+            getIngredients(element.ingredients);
+        });
+    };
+    
+    //get error
+    var getError = () => {
+    
+    };
+    
+    //get ingredients
+    var getIngredients = (ing)=>{
+        ing.forEach(item =>{
+            comuteHtml(item);
+        });
+    };
+    //comute html
+    var comuteHtml = (item)=>{
+        var result ="";
+        result+=`
+            <tr>
+                <td>${item.name}</td>
+                <td>${item.quantity}</td>
+                <td>${item.unit}</td>
+                <td><img src="${item.iconUrl}" class="img-fluit" width="100px"></td>
+            </tr>
+        `;
+        printOut(result);
+    }
+    //print out to html
+    var printOut = (out) =>{
+        $("#ingrediants").append(out);
+    };
 });
 
 // This is the fountion
