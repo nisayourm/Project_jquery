11111d21a6dca0eb4f78a48dd35426edcb253d88
nysar.yam <nysar.yam@PNC.LAN>
2019-12-13 20:44:53 +0700

    add new code

--- js/js.js
+++ js/js.js
@@ -1,131 +1,87 @@
-$(document).ready(function(){
+function getUrl() {
     var url = "https://raw.githubusercontent.com/radytrainer/test-api/master/test.json";
+    return url;
+}
+$(document).ready(function () {
+    getApi();
+    $('#recipe').on('change', () => {
+        var recipesId = $('#recipe').val();
+        eachRecipe(recipesId);
+    })
+})
+function getApi() {
     $.ajax({
-        dataType : 'json',
-        url:url,
-        success:function(data){
-            console.log(data);
-            var result = "";
-            data.recipes.forEach(element => { 
-                result +=`
-               
-                
-                  ${element.name}
-               
-                
-                <div class="card-body">
-                   
-                 <img src="${element.iconUrl}" width="130px" class="img-fluid"> 
-
-                </div>
-                   
-                `;
-            });
-            $('#code').append(result);
-        },
-        error: () => console.error("can not find"),
-    
+        dataType: 'json',
+        url: getUrl(),
+        success: (data) => chooseRecipe(data.recipes),
+        error: () => console.log("Canot get data"),
+    })
+}
+var allData = [];
+function chooseRecipe(recipe) {
+    allData = recipe;
+    var option = "";
+    recipe.forEach(element => {
+        option += `<option value ="${element.id}">${element.name}</option>`;
     });
-    // code for button input
-    $('#minusNumber').on('click',function(){
-        var minus = $('#number').val();
-        minusNumbers(minus);
-     });
+    $('#recipe').append(option);
+}
+function eachRecipe(id) {
+    allData.forEach(element => {
+        if (element.id == id) {
+            showRecipe(element);
+            // console.warn(element);
+            // showIngredient(element.ingredients);
+            showIngredient(element);
+        }
+    })
+}
+function showRecipe(element) {
+    const {name, iconUrl} = element;
+    var result = "";
+    result += `
+        <div class="col-2"></div>
+        <div class="col-4 mt-5"><h4>${name}</h4></div>
+        <div class="col-4"><img src="${iconUrl}"  style="width:200px; height:150px"></div>
+        <div class="col-2"></div>
+    `;
+    $('#nameOffood').html(result);
+}
 
-     $('#addNumber').on('click',function(){
-         var add = $('#number').val();
-         addNumber(add);
-     });
-     
-    // wertyuiop[]qwertyuiopwertyuiopweyuioqwertyuiopwertyuiopwertiop
-    $(document).ready(() => {
-        requestApi();
-    });
-    
-    //reques api
-    var requestApi = () => {
-        $.ajax({
-            dataType: 'json',
-            url: getUrl(),
-            success: (data) => getData(data),
-            error: () => getError(),
-        });
-    };
-    
-    //get url
-    var getUrl = () => {
-        var url = "https://raw.githubusercontent.com/radytrainer/test-api/master/test.json";
-        return url;
-    };
-    
-    //get data
-    var getData = (myData) => {
-        myData.recipes.forEach(element => {
-            //recipe element.ingredients
-            getIngredients(element.ingredients);
-        });
-    };
-    
-    //get error
-    var getError = () => {
-    
-    };
-    
-    //get ingredients
-    var getIngredients = (ing)=>{
-        ing.forEach(item =>{
-            comuteHtml(item);
-        });
-    };
-    //comute html
-    var comuteHtml = (item)=>{
-        var result ="";
-        result+=`
-            <tr>
-                <td>${item.name}</td>
-                <td>${item.quantity}</td>
-                <td>${item.unit}</td>
-                <td><img src="${item.iconUrl}" class="img-fluit" width="100px"></td>
-            </tr>
+// function showIngredient(ing) {
+//     ing.forEach(element => {
+//         showIngredients(element);
+//     })
+// }
+// function showIngredients(ids) {
+//     var incrdan = "";
+//     incrdan += `
+//              <tr>
+//                 <td><img src="${ids.iconUrl}" class="img-fluit"  width="100px"></td>
+//                 <br>
+//                 <br>
+//                 <td>${ids.name}</td>
+//                 <td>${ids.quantity}</td>
+//                 <td>${ids.unit[0]}</td>
+
+//             </tr>
+//     `;
+//     $('#recipe-result').html(incrdan);
+// }
+function showIngredient(ids) {
+    var incrdan = "";
+    ids.ingredients.forEach(element => {
+        const{name,quantity,unit,iconUrl} = element;
+        incrdan += `
+                 <tr>
+                    <td><img src="${iconUrl}" class="img-fluit"  width="100px"></td>
+                    <br>
+                    <br>
+                    <td>${name}</td>
+                    <td>${quantity}</td>
+                    <td>${unit}</td>
+                </tr>
         `;
-        printOut(result);
-    }
-    //print out to html
-    var printOut = (out) =>{
-        $("#ingrediants").append(out);
-    };
-});
-
-// This is the fountion
-
-function minusNumbers(minus){
-    var minuss = parseInt(minus)-1;
-    if(minuss >= 0){
-        $('#number').val(minuss);
-        compute(minuss);
-    }
-  
-}
-function compute(number){
-    var result = number *5;
-    if(number == 0){
-        progressBar(result);
-    }else{
-        progressBar(result+25);
-    }
-    $('#show').html(result);
-}
-function progressBar(pro){
-    $('#progress').width(pro+"%");
-    $('#progress').html(pro+"%");
-}
-
-function addNumber(add){
-    var addss = parseInt(add)+1;
-    if(addss <= 15){
-        $('#number').val(addss);
-        compute(addss);
-    }
-    $('#number').val(addss);
-  
+    });
+    $('#recipe-result').html(incrdan);
 }
